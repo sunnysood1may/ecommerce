c05e751ff3d671d0ac1978a22fec61224de2e884
sunny sood <sunny1may@gmail.com>
2024-12-08 21:43:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ecommerce Project</h1>
+      <button>Normal Button</button>
+      <Button>BootStrap Button</Button>
+    </div>
+  );
+}
+
+export default App;
